65a8bf563346b8252902d0a6c57e9bc56db3a3f3
Diego García <dagarciam1014@gmail.com>
2021-08-23 02:31:49 -0400

    Added support and overrides for Serilog

--- D_API/Program.cs
+++ D_API/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,18 @@ namespace D_API
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                   .MinimumLevel.Verbose()
+                   .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                   .Enrich.FromLogContext()
+                   .WriteTo.File("logs/.log", rollingInterval: RollingInterval.Hour, restrictedToMinimumLevel: LogEventLevel.Verbose)
+#if DEBUG
+                   .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Debug)
+#else
+                   .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Information)
+#endif
+                   .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
