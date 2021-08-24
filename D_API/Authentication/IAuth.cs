0513c0178f543fc8d75b9c31a9374b70bf20c5f6
Diego García <dagarciam1014@gmail.com>
2021-08-24 01:04:07 -0400

    Created IAuth interface

---
+++ D_API/Authentication/IAuth.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D_API.Authentication
+{
+    public interface IAuth
+    {
+        string Authenticate(string username, string password);
+    }
+}
