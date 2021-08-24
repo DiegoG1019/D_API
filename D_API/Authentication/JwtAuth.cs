b6e63f1052008910a7f7dfbe0bad5a4b8e59f072
Diego García <dagarciam1014@gmail.com>
2021-08-24 01:04:21 -0400

    Created JwTAuth class with test code

---
+++ D_API/Authentication/JwtAuth.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_API.Authentication
+{
+    public class JwtAuth : IAuth
+    {
+        private readonly string username = "kirtesh";
+        private readonly string password = "Demo1";
+        private readonly string key;
+        public JwtAuth(string key)
+        {
+            this.key = key;
+        }
+
+        public string Authenticate(string username, string password)
+        {
+            if (!(username.Equals(username) || password.Equals(password)))
+            {
+                return null;
+            }
+
+            // 1. Create Security Token Handler
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            // 2. Create Private Key to Encrypted
+            var tokenKey = Encoding.ASCII.GetBytes(key);
+
+            //3. Create JETdescriptor
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(
+                    new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name, username)
+                    }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.RsaSha256Signature)
+            };
+            //4. Create Token
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            // 5. Return Token from method
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
