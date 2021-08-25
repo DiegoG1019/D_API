e8316e57f411937cba1d34c2be304edcf9ceee5f
Diego García <dagarciam1014@gmail.com>
2021-08-25 15:59:29 -0400

    Created TestController

---
+++ D_API/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D_API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/test")]
+    public class TestController : ControllerBase
+    {
+    }
+}
