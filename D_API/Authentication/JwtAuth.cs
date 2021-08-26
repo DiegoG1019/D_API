3a17432258d72b60bebf5fcb852fe2c551ff8850
Diego García <dagarciam1014@gmail.com>
2021-08-25 21:24:55 -0400

    Added logging

--- D_API/Authentication/JwtAuth.cs
+++ D_API/Authentication/JwtAuth.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.IdentityModel.Tokens;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
@@ -34,6 +35,8 @@ namespace D_API.Authentication
                 Expires = null,
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
             };
+
+            Log.Warning($"Creating new Json Web Token: {name}, {role}");
 
             return tokenHandler.WriteToken(tokenHandler.CreateToken(tokenDescriptor));
         }
