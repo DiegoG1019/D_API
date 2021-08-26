ba760717d647fc8ab67ee653de88e83eaf408f72
Diego García <dagarciam1014@gmail.com>
2021-08-25 21:23:51 -0400

    Integrated initialization of settings

--- D_API/Program.cs
+++ D_API/Program.cs
@@ -1,3 +1,4 @@
+using DiegoG.Utilities.Settings;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -6,7 +7,9 @@ using Serilog;
 using Serilog.Events;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace D_API
@@ -15,6 +18,23 @@ namespace D_API
     {
         public static void Main(string[] args)
         {
+            Settings<APISettings>.Initialize(Directories.Configuration, "apisettings.cfg");
+
+            {
+                List<string> errors = new();
+                if (Settings<APISettings>.Current.APIKey is null)
+                    errors.Add($"Cannot start without a valid Telegram Bot API Key. Please configure one in {Path.GetFullPath(Directories.Configuration)}");
+
+                if (!Settings<APISettings>.Current.AllowedUsers.Any())
+                    errors.Add($"Cannot start without at least one allowed user. Please configure one in {Path.GetFullPath(Directories.Configuration)}");
+
+                if (Settings<APISettings>.Current.EventChannelId is 0)
+                    errors.Add($"Cannot start without a valid Event Telegram Channel Id. Please configure one in {Path.GetFullPath(Directories.Configuration)}");
+
+                if (errors.Any())
+                    throw new InvalidOperationException($"Found one or more errors when initializing the API:\n*> {string.Join("\n*> ", errors)}");
+            }
+
             Log.Logger = new LoggerConfiguration()
                    .MinimumLevel.Verbose()
                    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
@@ -27,6 +47,9 @@ namespace D_API
 #endif
                    .CreateLogger();
 
+            foreach (var x in typeof(Directories).GetFields(BindingFlags.Static | BindingFlags.Public))
+                Log.Information($"{x.Name} directory @ {x.GetValue(null)}");
+                
             CreateHostBuilder(args).Build().Run();
         }
 
