9de640ee2a7b7b87864bde745158a2216e183760
Diego García <dagarciam1014@gmail.com>
2021-08-26 21:43:44 -0400

    Introduced Program.cs to D_API.Test

---
+++ D_API.Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace D_API.Test
+{
+    static class Program
+    {
+    }
+}
