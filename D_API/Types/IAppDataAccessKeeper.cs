d7c064a85952444efd3bfa130384c5442da9b782
Diego García <dagarciam1014@gmail.com>
2021-08-28 00:17:36 -0400

    Added interface to keep track of file ownership

---
+++ D_API/Types/IAppDataAccessKeeper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D_API.Types
+{
+    public interface IAppDataAccessKeeper
+    {
+        public Task NewFile(string role, string file);
+        public Task<bool> CheckAccess(string role, string file);
+    }
+}
