fd6140f361fbe77d9567e041274c9e84bb670c88
Diego García <dagarciam1014@gmail.com>
2021-08-28 00:17:55 -0400

    Created AppDataAccessFileKeeper

---
+++ D_API/Types/AppDataAccessFileKeeper.cs
@@ -0,0 +1,28 @@
+﻿using DiegoG.Utilities.IO;
+using Nito.AsyncEx;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D_API.Types
+{
+    public class AppDataAccessFileKeeper : IAppDataAccessKeeper
+    {
+        public AppDataAccessFileKeeper(string filename)
+        {
+
+        }
+
+        public Task<bool> CheckAccess(string role, string file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task NewFile(string role, string file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
