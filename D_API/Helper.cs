9e78cee84c291e07d4305edbfd7dbf7fdc5ae2b3
Diego García <dagarciam1014@gmail.com>
2021-08-28 23:29:03 -0400

    Made CheckAuthValidity async

--- D_API/Helper.cs
+++ D_API/Helper.cs
@@ -9,14 +9,15 @@ namespace D_API
 {
     public static class Helper
     {
-        public static void CheckAuthValidity(this ClaimsPrincipal user)
+        public static async Task CheckAuthValidity(this ClaimsPrincipal user)
         {
             if (user.Identity?.Name is null)
-            {
-                string d = $"An authorized user cannot have a null Identity or Name. Claims: \n*> {string.Join("\n*>", user.Claims.Select(x => $"Type: {x.Type}, Value: {x.Value}, Issuer: {x.Issuer}"))}";
-                Log.Error(d);
-                throw new InvalidOperationException(d);
-            }
+                await Task.Run(() =>
+                {
+                    string d = $"An authorized user cannot have a null Identity or Name. Claims: \n*> {string.Join("\n*>", user.Claims.Select(x => $"Type: {x.Type}, Value: {x.Value}, Issuer: {x.Issuer}"))}";
+                    Log.Error(d);
+                    throw new InvalidOperationException(d);
+                });
         }
     }
 }
