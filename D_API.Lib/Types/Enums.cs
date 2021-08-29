31730b4d3d10b0be8b739ea7e5a12fd60c8822b1
Diego García <dagarciam1014@gmail.com>
2021-08-29 19:34:48 -0400

    Added Roles

--- D_API.Lib/Types/Enums.cs
+++ D_API.Lib/Types/Enums.cs
@@ -4,4 +4,12 @@ using System.Text;
 
 namespace D_API.Lib.Types
 {
+    public enum Roles
+    {
+        Unauthenticated = -1,
+        None = 0,
+        Moderator = 1,
+        Administrator = 2,
+        Root = 3
+    }
 }
