c5deb944716e96008a0fd417263d82ae6a7c73ad
Diego García <dagarciam1014@gmail.com>
2021-08-28 23:24:02 -0400

    Added Helper method static class

---
+++ D_API/Helper.cs
@@ -0,0 +1,13 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace D_API
+{
+    public static class Helper
+    {
+    }
+}
