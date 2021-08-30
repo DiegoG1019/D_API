834e65c680289716859f072e3ca206fc72f9dbd2
Diego García <dagarciam1014@gmail.com>
2021-08-30 02:31:59 -0400

    Made all requests go through the request queue

--- D_API.Lib/D_APIClient.cs
+++ D_API.Lib/D_APIClient.cs
@@ -16,12 +16,15 @@ namespace D_API.Lib
 
         protected readonly HttpClient Http;
         protected readonly D_APIClientConfig Config;
+        protected readonly IRequestQueue RequestQueue;
 
         public D_APIClient(Uri address, string key, D_APIClientConfig? config = null)
         {
             Http = new HttpClient() { BaseAddress = address };
             Http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", key);
             Config = config ?? new D_APIClientConfig();
+
+            RequestQueue = Config.AutoQueue ? (IRequestQueue)new D_APIRequestQueue() : new D_APINoRequestQueue();
         }
         public D_APIClient(string address, string key) : this(new Uri(address), key) { }
 
@@ -33,11 +36,13 @@ namespace D_API.Lib
             if (key != null)
                 Http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", key);
             Config = config ?? new D_APIClientConfig();
+
+            RequestQueue = Config.AutoQueue ? (IRequestQueue)new D_APIRequestQueue() : new D_APINoRequestQueue();
         }
         public D_APIClient(HttpClient client, string? address = null, string? key = null) : this(client, address != null ? new Uri(address) : null, key) { }
 
         private Role? RoleField;
-        public async Task<Role> GetRole()
+        public Task<Role> GetRole() => RequestQueue.NewRequest(async () =>
         {
             using (await Lock.LockAsync())
             {
@@ -45,14 +50,14 @@ namespace D_API.Lib
                     RoleField = await Http.GetFromJsonAsync<Role>("api/test/proberole");
                 return (Role)RoleField;
             }
-        }
+        });
 
         public Task<bool> ProbeAuthRoot() => Probe_("probeauthroot");
         public Task<bool> ProbeAuthAdmin() => Probe_("probeauthadmin");
         public Task<bool> ProbeAuthMod() => Probe_("probeauthmod");
         public Task<bool> ProbeAuth() => Probe_("probeauth");
         public Task<bool> Probe() => Probe_("probe");
-        private async Task<bool> Probe_(string endpoint)
+        private Task<bool> Probe_(string endpoint) => RequestQueue.NewRequest(async () =>
         {
             try
             {
@@ -62,21 +67,21 @@ namespace D_API.Lib
             {
                 return false;
             }
-        }
+        });
 
-        public async Task<T> GetAppData<T>(string appname)
+        public Task<T> GetAppData<T>(string appname) => RequestQueue.NewRequest(async () =>
         {
             var r = await Http.GetAsync($"api/v1/appdatahost/config/{appname}");
             if (r.StatusCode is HttpStatusCode.OK)
-                return await r.Content.ReadFromJsonAsync<T>();
+                return (await r.Content.ReadFromJsonAsync<T>())!;
             if (r.StatusCode is HttpStatusCode.Forbidden)
                 throw new D_APIException($"This client is not logged in as the owner of {appname}");
             if (r.StatusCode is HttpStatusCode.NotFound)
                 throw new D_APIException($"There is no data under {appname}");
             throw new D_APIException("Unknown");
-        }
+        });
 
-        public Task PostAppData<T>(string appname, T data, bool overwrite = false)
-            => Http.PostAsJsonAsync($"api/v1/appdatahost/config/{appname}/{overwrite}", data);
+        public Task PostAppData<T>(string appname, T data, bool overwrite = false) 
+            => RequestQueue.NewRequest(() => Http.PostAsJsonAsync($"api/v1/appdatahost/config/{appname}/{overwrite}", data));
     }
 }
