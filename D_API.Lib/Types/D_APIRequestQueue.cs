8088284695d78ae5abba8f3d7c327a96c219c405
Diego García <dagarciam1014@gmail.com>
2021-08-29 23:09:50 -0400

    Create D_APIRequestQueue.cs

---
+++ D_API.Lib/Types/D_APIRequestQueue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace D_API.Lib.Types
+{
+    public class D_APIRequestQueue : IRequestQueue
+    {
+        public Task<T> NewRequest<T>(Func<Task<T>> func)
+        {
+
+        }
+
+        public Task<T> NewRequest<T>(Func<T> func)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task NewRequest(Func<Task> func)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task NewRequest(Action func)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
