d9e5162e2fcfc7b9d21651e9792a28d7c17285b9
Diego García <dagarciam1014@gmail.com>
2021-08-29 23:10:42 -0400

    Added constructor and fields to D_APIRequestQueue

--- D_API.Lib/Types/D_APIRequestQueue.cs
+++ D_API.Lib/Types/D_APIRequestQueue.cs
@@ -8,6 +8,31 @@ namespace D_API.Lib.Types
 {
     public class D_APIRequestQueue : IRequestQueue
     {
+        private readonly Task Running;
+
+        private readonly Queue<DateTime> ImmediateRequests = new Queue<DateTime>(); //These are the seconds based ones
+
+        private readonly IReadOnlyList<(Queue<DateTime> Dates, int Max)> Requests = new List<(Queue<DateTime>, int)>
+        {
+            (new Queue<DateTime>(), 80), // 1 : Minutes
+            (new Queue<DateTime>(), 25_000), // 2 : 12h
+            (new Queue<DateTime>(), 100_000)  // 3 : 7 days
+        };
+
+        private readonly Queue<Func<Task>> QueuedTasks = new Queue<Func<Task>>();
+
+        protected readonly CancellationTokenSource TokenSource = new CancellationTokenSource();
+
+        public D_APIRequestQueue()
+        {
+            Running = new Task(async () => await Run(TokenSource.Token), TaskCreationOptions.LongRunning);
+            Running.Start();
+        }
+
+        protected virtual async Task Run(CancellationToken cancellationToken)
+        {
+        }
+
         public Task<T> NewRequest<T>(Func<Task<T>> func)
         {
 
