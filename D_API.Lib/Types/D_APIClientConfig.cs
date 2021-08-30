197593fba202a90a9601400f5aeb47ba81525dab
Diego García <dagarciam1014@gmail.com>
2021-08-29 23:09:07 -0400

    Created D_APIClientConfig

---
+++ D_API.Lib/Types/D_APIClientConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D_API.Lib.Types
+{
+    public class D_APIClientConfig
+    {
+        public readonly bool AutoQueue;
+
+        public D_APIClientConfig(bool autoQueue = true)
+        {
+            AutoQueue = autoQueue;
+        }
+    }
+}
