5344247e613bc58416dec3580d82983468a066b4
Diego García <dagarciam1014@gmail.com>
2021-08-30 02:31:48 -0400

    Create D_APINoRequestQueue.cs

---
+++ D_API.Lib/Types/D_APINoRequestQueue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_API.Lib.Types
+{
+    public class D_APINoRequestQueue : IRequestQueue
+    {
+        public Task<T> NewRequest<T>(Func<Task<T>> func) => func();
+
+        public Task<T> NewRequest<T>(Func<T> func) => Task.Run(func);
+
+        public Task NewRequest(Func<Task> func) => func();
+
+        public Task NewRequest(Action func) => Task.Run(func);
+    }
+}
