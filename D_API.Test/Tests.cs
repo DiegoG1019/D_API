0a018becec80cff690bcb1a4a3c4e331e14ffadb
Diego García <dagarciam1014@gmail.com>
2021-08-30 02:05:56 -0400

    Got rid of NUnit

--- D_API.Test/Tests.cs
+++ D_API.Test/Tests.cs
@@ -1,6 +1,5 @@
 using D_API.Lib;
 using DiegoG.Utilities.Settings;
-using NUnit.Framework;
 using System.Threading.Tasks;
 using static System.Console;
 using static D_API.Test.Helper;
@@ -12,29 +11,28 @@ namespace D_API.Test
 {
     public class Tests
     {
-        public D_APIClient Client { get; private set; }
-        [SetUp]
-        public void Setup()
+        public static D_APIClient Client { get; private set; }
+
+        public static async Task Main()
         {
             Settings<TestSettings>.Initialize(".config", "settings");
             Write("Insert APIKey\n> ");
             Client = new(Settings<TestSettings>.Current.BaseAddress ?? "https://localhost:44379/", ReadLine());
         }
 
-        [Test(Author = "Diego Garcia", Description = "Bombard the Rate Limiting Queue to see if it works", ExpectedResult = "The Queue eventually clearing")]
-        public async Task TestQueue()
+        public static async Task<bool> TestQueue()
         {
             Task[] tasks = new Task[30];
             for (int i = 0; i < 30; i++)
                 tasks[i] = Client.Probe();
             await Task.Delay(2000);
             await Task.WhenAll(tasks);
+            return true;
         }
 
-        [Test(Author = "Diego Garcia", Description = "Tests if AppData works as intended", ExpectedResult = "Passing")]
-        public async Task TestAppData()
+        public static async Task<bool> TestAppData()
         {
-            await Task.Run(async () =>
+            return await Task.Run(async () =>
             {
                 try
                 {
@@ -43,6 +41,7 @@ namespace D_API.Test
                 catch(D_APIException exc)
                 {
                     Helper.StoreResult($"Get Test 1 failed: {exc.Message}");
+                    return false;
                 }
 
                 try
@@ -53,6 +52,7 @@ namespace D_API.Test
                 catch (D_APIException exc)
                 {
                     Helper.StoreResult($"Post Test 1 failed: {exc.Message}");
+                    return false;
                 }
 
                 try
@@ -62,35 +62,35 @@ namespace D_API.Test
                 catch (D_APIException exc)
                 {
                     Helper.StoreResult($"Get Test 2 failed: {exc.Message}");
+                    return false;
                 }
 
                 try
                 {
                     await Client.PostAppData("Test", "papppasdadv", false);
                     Helper.StoreResult($"Post Test 2 (ow:true): FAILED");
+                    return false;
                 }
                 catch (Exception exc)
                 {
                     Helper.StoreResult($"Get Test 2: {exc.Message}");
                 }
-            }).AwaitWithTimeout(10000, null, () => Assert.Fail($"Probing took too long to complete"));
 
-            Assert.Pass();
+                return true;
+            }).AwaitWithTimeout(10000, null, () => throw new Exception($"Probing took too long to complete"));
         }
 
-        [Test(Author = "Diego Garcia", Description = "Tests the probing effectiveness of the Client", ExpectedResult = "Passing")]
-        public async Task TestProbe()
+        public static async Task<bool> TestProbe()
         {
-            await Task.Run(async () =>
+            return await Task.Run(async () =>
             {
                 Helper.StoreResult($"Probe: {await Client.Probe()}");
                 Helper.StoreResult($"ProbeAuth: {await Client.ProbeAuth()}");
                 Helper.StoreResult($"ProbeAuthMod: {await Client.ProbeAuthMod()}");
                 Helper.StoreResult($"ProbeAuthAdmin: {await Client.ProbeAuthAdmin()}");
                 Helper.StoreResult($"ProbeAuthRoot: {await Client.ProbeAuthRoot()}");
-            }).AwaitWithTimeout(5000, null, ()=> Assert.Fail($"Probing took too long to complete"));
-            
-            Assert.Pass();
+                return true;
+            }).AwaitWithTimeout(5000, null, () => throw new Exception($"Probing took too long to complete"));
         }
     }
 }
