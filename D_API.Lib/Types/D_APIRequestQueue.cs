4eb8e3fc87b6106687295548183bbde12e374bce
Diego García <dagarciam1014@gmail.com>
2021-08-29 23:10:55 -0400

    Implemented Run method

--- D_API.Lib/Types/D_APIRequestQueue.cs
+++ D_API.Lib/Types/D_APIRequestQueue.cs
@@ -31,6 +31,37 @@ namespace D_API.Lib.Types
 
         protected virtual async Task Run(CancellationToken cancellationToken)
         {
+            while(!cancellationToken.IsCancellationRequested)
+            {
+            WhileContinue:;
+                await Task.Delay(500);
+
+                //   Available requests
+                // ----------------------
+
+                if (QueuedTasks.Count is 0)
+                    continue;
+
+                int x = 5 - ImmediateRequests.Count;
+                if (x <= 0)
+                    goto WhileContinue;
+                foreach (var (Dates, Max) in Requests)
+                {
+                    var v = Max - Dates.Count;
+                    if (v <= 0)
+                        goto WhileContinue;
+                    if (v < x)
+                        x = v;
+                }
+
+                //   Execute Requests
+                // --------------------
+
+                var arr = new Task[x];
+                for (int i = 0; i < x && QueuedTasks.Count > 0; i++)
+                    arr[i] = QueuedTasks.Dequeue()();
+                await Task.WhenAll(arr);
+            }
         }
 
         public Task<T> NewRequest<T>(Func<Task<T>> func)
