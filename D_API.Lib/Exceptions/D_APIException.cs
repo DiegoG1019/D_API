5c894f0360f48464a8ca5f15c4a37e1c7e7b339a
Diego García <dagarciam1014@gmail.com>
2021-08-29 23:09:14 -0400

    Create D_APIException.cs

---
+++ D_API.Lib/Exceptions/D_APIException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D_API.Lib.Exceptions
+{
+    [Serializable]
+    public class D_APIException : Exception
+    {
+        public D_APIException() { }
+        public D_APIException(string message) : base(message) { }
+        public D_APIException(string message, Exception inner) : base(message, inner) { }
+        protected D_APIException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
