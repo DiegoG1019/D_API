4bffb59be1bb39a6d2e3d41396814216d78a661b
Diego García <dagarciam1014@gmail.com>
2021-08-29 23:09:18 -0400

    Create IRequestQueue.cs

---
+++ D_API.Lib/Types/IRequestQueue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_API.Lib.Types
+{
+    public interface IRequestQueue
+    {
+        Task<T> NewRequest<T>(Func<Task<T>> func);
+        Task<T> NewRequest<T>(Func<T> func);
+        Task NewRequest(Func<Task> func);
+        Task NewRequest(Action func);
+    }
+}
