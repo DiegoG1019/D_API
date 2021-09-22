50313a4e7bab61491992effd7cb5bbc9e17d6886
Diego García <dagarciam1014@gmail.com>
2021-09-21 22:59:29 -0400

    Create IAuthCredentialsVerificationProvider.cs

---
+++ D_API/Interfaces/IAuthCredentialsVerificationProvider.cs
@@ -0,0 +1,11 @@
+﻿using D_API.Types.Auth;
+using D_API.Types.Auth.Models;
+
+namespace D_API.Interfaces;
+
+public interface IAuthCredentialsVerificationProvider
+{
+    public Task<CredentialVerificationResults> Verify(ClientValidCredentials credentials);
+
+    public Task<Client?> FindClient(Guid key);
+}
