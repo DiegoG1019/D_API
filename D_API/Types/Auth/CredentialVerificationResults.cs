6424b02764556294256b54d8326e622e3cfa1b1f
Diego García <dagarciam1014@gmail.com>
2021-09-21 23:00:13 -0400

    Added new models

---
+++ D_API/Types/Auth/CredentialVerificationResults.cs
@@ -0,0 +1,13 @@
+﻿using D_API.Types.Auth.Models;
+
+namespace D_API.Types.Auth;
+
+public sealed record CredentialVerificationResults(CredentialVerificationResult Result, Client? Client) { }
+
+public enum CredentialVerificationResult
+{
+    Forbidden,
+    Revoked,
+    Unauthorized,
+    Authorized,
+}
