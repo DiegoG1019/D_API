5f376a847043d6e677067bf4e5ec81ad1d8a6669
Diego García <dagarciam1014@gmail.com>
2021-09-21 23:01:51 -0400

    Updated

--- D_API/Program.cs
+++ D_API/Program.cs
@@ -1,82 +1,93 @@
-using DiegoG.TelegramBot;
-using DiegoG.Utilities.Settings;
+global using DiegoG.TelegramBot;
+global using DiegoG.Utilities.Settings;
+global using Serilog;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using System;
+global using System.Collections.Generic;
+global using DiegoG.Utilities.IO;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Serilog;
 using Serilog.Events;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
+using D_API.Types.Utilities;
 
-namespace D_API
+namespace D_API;
+
+public static partial class Program
 {
-    public static partial class Program
+    private readonly static Stopwatch stopwatch = new();
+    private readonly static Stopwatch startwatch = new();
+
+    public readonly static Version Version = Assembly.GetExecutingAssembly().GetName().Version!;
+
+    public static TimeSpan Runtime => stopwatch.Elapsed;
+    public static TimeSpan StartupTime { get; private set; }
+
+    public static TelegramBotCommandClient TelegramBot { get; private set; }
+
+    public static void Main(string[] args)
     {
-        private readonly static Stopwatch stopwatch = new();
-        private readonly static Stopwatch startwatch = new();
+        startwatch.Start();
+        
+        Settings<APISettings>.Initialize(Directories.Configuration, "apisettings.cfg");
 
-        public readonly static Version Version = Assembly.GetExecutingAssembly().GetName().Version!;
-
-        public static TimeSpan Runtime => stopwatch.Elapsed;
-        public static TimeSpan StartupTime { get; private set; }
-
-        public static TelegramBotCommandClient TelegramBot { get; private set; }
-        public static void Main(string[] args)
         {
-            startwatch.Start();
-            Settings<APISettings>.Initialize(Directories.Configuration, "apisettings.cfg");
+            List<string> errors = new();
+            if (Settings<APISettings>.Current.TelegramAPIKey is null)
+                errors.Add($"Cannot start without a valid Telegram Bot API Key. Please configure one in {Path.GetFullPath(Directories.Configuration)}");
 
-            {
-                List<string> errors = new();
-                if (Settings<APISettings>.Current.APIKey is null)
-                    errors.Add($"Cannot start without a valid Telegram Bot API Key. Please configure one in {Path.GetFullPath(Directories.Configuration)}");
+            if (!Settings<APISettings>.Current.AllowedUsers.Any())
+                errors.Add($"Cannot start without at least one allowed user. Please configure one in {Path.GetFullPath(Directories.Configuration)}");
 
-                if (!Settings<APISettings>.Current.AllowedUsers.Any())
-                    errors.Add($"Cannot start without at least one allowed user. Please configure one in {Path.GetFullPath(Directories.Configuration)}");
+            if (Settings<APISettings>.Current.EventChannelId is 0)
+                errors.Add($"Cannot start without a valid Event Telegram Channel Id. Please configure one in {Path.GetFullPath(Directories.Configuration)}");
 
-                if (Settings<APISettings>.Current.EventChannelId is 0)
-                    errors.Add($"Cannot start without a valid Event Telegram Channel Id. Please configure one in {Path.GetFullPath(Directories.Configuration)}");
-
-                if (errors.Any())
-                    throw new InvalidOperationException($"Found one or more errors when initializing the API:\n*> {string.Join("\n*> ", errors)}");
-            }
-
-            TelegramBot = new(Settings<APISettings>.Current.APIKey!, 10, 
-                config: new(true, true, false, true, true), 
-                messageFilter: m => Settings<APISettings>.Current.AllowedUsers.Contains(m.From.Id));
-            {
-                var c = new LoggerConfiguration()
-                       .MinimumLevel.Verbose()
-                       .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                       .Enrich.FromLogContext()
-                       .WriteTo.File(Directories.InLogs(".log"), rollingInterval: RollingInterval.Hour, restrictedToMinimumLevel: LogEventLevel.Verbose)
-#if DEBUG
-                       .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Debug)
-#else
-                       .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Information)
-#endif
-                       ;
-                
-                if(Settings<APISettings>.Current.EventChannelId is not null)
-                    c.WriteTo.TelegramBot(Settings<APISettings>.Current.EventChannelId, TelegramBot, LogEventLevel.Warning, "API");
-
-                Log.Logger = c.CreateLogger();
-            }
-
-            foreach (var x in typeof(Directories).GetFields(BindingFlags.Static | BindingFlags.Public))
-                Log.Information($"{x.Name} directory @ {x.GetValue(null)}");
-
-            CreateHostBuilder(args).Build().Run();
+            if (errors.Any())
+                throw new InvalidOperationException($"Found one or more errors when initializing the API:\n*> {string.Join("\n*> ", errors)}");
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+        TelegramBot = new(Settings<APISettings>.Current.TelegramAPIKey!, 10,
+            config: new(true, true, false, true, true),
+            messageFilter: m => Settings<APISettings>.Current.AllowedUsers.Contains(m.From.Id));
+        {
+            var c = new LoggerConfiguration()
+                   .MinimumLevel.Verbose()
+                   .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                   .Enrich.FromLogContext()
+                   .WriteTo.File(Directories.InLogs(".log"), rollingInterval: RollingInterval.Hour, restrictedToMinimumLevel: LogEventLevel.Verbose)
+#if DEBUG
+                   .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Debug)
+#else
+                   .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Information)
+#endif
+                   ;
+
+            if (Settings<APISettings>.Current.EventChannelId is not null)
+                c.WriteTo.TelegramBot(Settings<APISettings>.Current.EventChannelId, TelegramBot, LogEventLevel.Warning,
+#if !DEBUG
+                    "API"
+#else
+                    "Debug_API"
+#endif
+
+                    );
+
+            Log.Logger = c.CreateLogger();
+        }
+
+        foreach (var x in typeof(Directories).GetFields(BindingFlags.Static | BindingFlags.Public))
+            Log.Information($"{x.Name} directory @ {x.GetValue(null)}");
+
+        CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
 }
--- D_API/Startup.cs
+++ D_API/Startup.cs
@@ -1,131 +1,228 @@
 using AspNetCoreRateLimit;
 using D_API.Authentication;
-using DiegoG.Utilities.Settings;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using Serilog;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using D_API.Types;
 using Microsoft.AspNetCore.HttpOverrides;
-using System.Net;
+using D_API.Interfaces;
 
-namespace D_API
+namespace D_API;
+
+public static partial class Program
 {
-    public static partial class Program
+    public class Startup
     {
-        public static IAuth Auth { get; private set; }
-        public class Startup
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
         {
-            public Startup(IConfiguration configuration)
+            string key = GetKey();
+
+            services.AddOptions();
+            services.AddMemoryCache();
+            services.Configure<ClientRateLimitOptions>(Configuration.GetSection("ClientRateLimiting"));
+            services.Configure<ClientRateLimitOptions>(Configuration.GetSection("ClientRateLimitPolicies"));
+
+            services.AddInMemoryRateLimiting();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
+            //services.Configure<ForwardedHeadersOptions>(options =>
+            //{
+            //    options.KnownProxies.Add(IPAddress.Parse("10.0.0.100"));
+            //}); //Not necessary as of now
+
+            var jwtIssuer = Settings<APISettings>.Current.Security.Issuer;
+            var jwtAudience = Settings<APISettings>.Current.Security.Audience;
+            var jwtSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key));
+
+            services.AddControllers();
+            services.AddAuthentication(x =>
             {
-                Configuration = configuration;
-            }
-
-            public IConfiguration Configuration { get; }
-
-            // This method gets called by the runtime. Use this method to add services to the container.
-            public void ConfigureServices(IServiceCollection services)
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
             {
-                string key;
-                {
-                    var args = Environment.GetCommandLineArgs();
-                    if (args.Length is > 1)
-                        key = args[1];
-                    else
-                    {
-                        Log.Error("Could not obtain a security key from command line arguments. Using default key instead.");
-                        key = "qgBVG:Qv7W?ns7Rf_eRdA2J,~NayIrYKr?X%PX@YcOi!IgHV@5Ln:jeGYVb1Smc+";
-                    }
-                }
-
-                services.AddOptions();
-                services.AddMemoryCache();
-                services.Configure<ClientRateLimitOptions>(Configuration.GetSection("ClientRateLimiting"));
-                services.Configure<ClientRateLimitOptions>(Configuration.GetSection("ClientRateLimitPolicies"));
-
-                services.AddInMemoryRateLimiting();
-
-                services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-                services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
-
-                //services.Configure<ForwardedHeadersOptions>(options =>
-                //{
-                //    options.KnownProxies.Add(IPAddress.Parse("10.0.0.100"));
-                //}); //Not necessary as of now
-
-                services.AddControllers();
-                services.AddAuthentication(x =>
-                {
-                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                }).AddJwtBearer(x =>
-                {
 #if DEBUG
-                    x.RequireHttpsMetadata = false;
+                x.RequireHttpsMetadata = false;
 #endif
-                    x.Audience = Settings<APISettings>.Current.Security.Audience;
-                    x.SaveToken = true;
-                    x.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        ValidateIssuer = false,
-                        ValidateAudience = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key))
-                    };
-                });
+                x.Audience = jwtAudience;
+                x.ClaimsIssuer = jwtIssuer;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = jwtIssuer is not null,
+                    ValidateAudience = jwtAudience is not null,
+                    RequireExpirationTime = true,
+                    RequireAudience = jwtAudience is not null,
+                    RequireSignedTokens = true,
+                    ClockSkew = TimeSpan.Zero,
+                    IssuerSigningKey = jwtSigningKey,
+                };
+            });
 
-                Auth = new JwtAuth(key);
-                services.AddSingleton<IAuth>(Auth);
-                services.AddSingleton<IAppDataAccessKeeper>(new AppDataAccessFileKeeper("main"));
-                services.AddControllers();
-                services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "D_API", Version = "v1" }));
+            services.AddSingleton<IJwtProvider>(new JwtAuth(jwtSigningKey, jwtAudience, jwtIssuer));
+            services.AddSingleton<IAppDataAccessKeeper>(new AppDataAccessFileKeeper("main"));
+            {
+                var (hashkey, enkey, eniv) = GetMCVPData();
+                services.AddSingleton<IAuthCredentialsVerificationProvider>(new MemoryCredentialsVerificationProvider(hashkey, enkey, eniv));
+            }
+            services.AddControllers();
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "D_API", Version = "v1" }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "D_API v1"));
             }
 
-            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-            {
-                app.UseForwardedHeaders(new ForwardedHeadersOptions
-                {
-                    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-                });
+            app.UseClientRateLimiting();
 
-                if (env.IsDevelopment())
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+            startwatch.Stop();
+            StartupTime = startwatch.Elapsed;
+            stopwatch.Start();
+        }
+
+        private static (string, string?, string?) GetMCVPData()
+        {
+            return (GetHashKey(), GetEncryptionKey(), GetEncryptionIV());
+
+            static string? GetEncryptionIV()
+            {
+                if (Settings<APISettings>.Current.EncryptionIV is not null)
                 {
-                    app.UseDeveloperExceptionPage();
-                    app.UseSwagger();
-                    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "D_API v1"));
+                    Log.Information($"Obtained Encryption IV from configurations file");
+                    return Settings<APISettings>.Current.EncryptionIV;
                 }
 
-                app.UseClientRateLimiting();
+                string[] args = Environment.GetCommandLineArgs();
+                if (args.Length is >= 4)
+                {
+                    Log.Information($"Obtained Encryption IV from Command Line Arguments file");
+                    return args[3];
+                }
 
-                app.UseHttpsRedirection();
+                string? envkey;
+                if ((envkey = Environment.GetEnvironmentVariable("EncryptionIV")) is not null)
+                {
+                    Log.Information("Obtained Encryption IV from an environment variable");
+                    return envkey;
+                }
 
-                app.UseRouting();
-
-                app.UseAuthentication();
-
-                app.UseAuthorization();
-
-                app.UseEndpoints(endpoints => endpoints.MapControllers());
-
-                startwatch.Stop();
-                StartupTime = startwatch.Elapsed;
-                stopwatch.Start();
+                Log.Error("Could not obtain a Encryption IV from configurations file, command line arguments or an environment varible. No IV available.");
+                return null;
             }
+
+            static string? GetEncryptionKey()
+            {
+                if (Settings<APISettings>.Current.EncryptionKey is not null)
+                {
+                    Log.Information($"Obtained Encryption Key from configurations file");
+                    return Settings<APISettings>.Current.EncryptionKey;
+                }
+
+                string[] args = Environment.GetCommandLineArgs();
+                if (args.Length is >= 3)
+                {
+                    Log.Information($"Obtained Encryption Key from Command Line Arguments file");
+                    return args[2];
+                }
+
+                string? envkey;
+                if ((envkey = Environment.GetEnvironmentVariable("EncryptionKey")) is not null)
+                {
+                    Log.Information("Obtained Encryption Key from an environment variable");
+                    return envkey;
+                }
+
+                Log.Error("Could not obtain a Encryption Key from configurations file, command line arguments or an environment varible. No Encryption Key available");
+                return null;
+            }
+
+            static string GetHashKey()
+            {
+                if (Settings<APISettings>.Current.ClientSecretHashKey is not null)
+                {
+                    Log.Information($"Obtained Client Secret HashKey from configurations file");
+                    return Settings<APISettings>.Current.ClientSecretHashKey;
+                }
+
+                string[] args = Environment.GetCommandLineArgs();
+                if(args.Length is >= 2)
+                {
+                    Log.Information($"Obtained Client Secret HashKey from Command Line Arguments file");
+                    return args[1];
+                }
+
+                string? envkey;
+                if ((envkey = Environment.GetEnvironmentVariable("SecretHashKey")) is not null)
+                {
+                    Log.Information("Obtained Client Secret HashKey from an environment variable");
+                    return envkey;
+                }
+
+                Log.Error("Could not obtain a Client Secret HashKey from configurations file, command line arguments or an environment varible. Using default key instead.");
+                return "&fCd>2Cpxz=@SK>^sQkt5zV0aE]8IKNsqazFOPb:m-RBq0VsBSN?Ebn&^aiO6wE@";
+            }
+        }
+
+        private static string GetKey()
+        {
+            if (Settings<APISettings>.Current.JWTSecurityKey is not null)
+            {
+                Log.Information("Obtained JWT Security key from configurations file");
+                return Settings<APISettings>.Current.JWTSecurityKey;
+            }
+
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length is >= 1)
+            {
+                Log.Information("Obtained JWT Security key from Command Line arguments");
+                return args[0];
+            }
+
+            string? envkey;
+            if ((envkey = Environment.GetEnvironmentVariable("JsonWebTokenKey")) is not null)
+            {
+                Log.Information("Obtained JWT Security key from an environment variable");
+                return envkey;
+            } 
+                
+            Log.Error("Could not obtain a JWT Security key from configurations file, command line arguments or an environment varible. Using default key instead.");
+            return "qgBVG:Qv7W?ns7Rf_eRdA2J,~NayIrYKr?X%PX@YcOi!IgHV@5Ln:jeGYVb1Smc+";
         }
     }
 }
