175d53379dd76ca34a880ad68a663e9dc14238c6
Diego García <dagarciam1014@gmail.com>
2021-09-21 22:59:54 -0400

    Moved interface types

---
+++ D_API/Interfaces/IAppDataAccessKeeper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace D_API.Interfaces;
+
+public interface IAppDataAccessKeeper
+{
+    public Task NewFile(ClaimsPrincipal id, string file);
+    public Task<bool> CheckAccess(ClaimsPrincipal id, string file);
+}
