87eb7d634aa9f51eccca2d68b199a5ceec9ad18a
Diego García <dagarciam1014@gmail.com>
2021-09-27 16:12:31 -0400

    Create UsageTracker.cs

---
+++ D_API/Models/DataKeeper/DataUsage/Complex/UsageTracker.cs
@@ -0,0 +1,24 @@
+﻿using DiegoG.Utilities.Measures;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace D_API.Models.DataKeeper.DataUsage.Complex;
+
+[ComplexType]
+public class UsageTracker
+{
+    public UsageTracker(DataMeasure download, DataMeasure upload)
+    {
+        Upload = upload;
+        Download = download;
+    }
+
+    public UsageTracker() : this(new DataMeasure(), new DataMeasure()) { }
+    public UsageTracker(double download, double upload) : this(new DataMeasure(download), new DataMeasure(upload)) { }
+    public UsageTracker(double download, Data.DataPrefix prefixDownload, double upload, Data.DataPrefix prefixUpload)
+        : this(new DataMeasure(download, prefixDownload), new DataMeasure(upload, prefixUpload)) { }
+
+    public DataMeasure Upload { get; set; }
+
+    public DataMeasure Download { get; set; }
+}
