a250af82c9908f18bd1c47caddca49460db2945b
Diego García <dagarciam1014@gmail.com>
2021-09-27 16:12:18 -0400

    Create ClientDataTracker.cs

---
+++ D_API/Models/DataKeeper/ClientDataTracker.cs
@@ -0,0 +1,38 @@
+﻿using D_API.Models.DataKeeper.DataUsage.Complex;
+using DiegoG.Utilities.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace D_API.Models.DataKeeper;
+
+public class ClientDataTracker
+{
+    [Key]
+    public Guid Key { get; set; }
+
+    public UsageTracker DailyTransferQuota { get; set; } = new();
+
+    [ConcurrencyCheck]
+    public DailyUsageTracker DailyTransferUsage { get; set; } = new();
+
+    public DataMeasure StorageQuota { get; set; } = new();
+
+    public DataMeasure StorageUsage { get; set; } = new();
+
+    [NotMapped]
+    public bool IsOverStorageQuota => StorageUsage >= StorageQuota;
+    public double WouldSurpassStorageQuota(double data) => WouldSurpassStorageQuota(new DataMeasure(data));
+    public double WouldSurpassStorageQuota(DataMeasure data)
+    {
+        var r = StorageUsage + data - StorageQuota;
+        return r > DiegoG.Utilities.Measures.Data.Zero ? r.TotalBytes : 0;
+    }
+
+    public async Task<(bool IsOverDownload, bool IsOverUpload, double OverDownloadQuota, double OverUploadQuota)> GetIfOverTransferQuota()
+    {
+        var total = await DailyTransferUsage.GetTotal();
+        double odq = total.Download.TotalBytes - DailyTransferQuota.Download.TotalBytes;
+        double ouq = total.Upload.TotalBytes - DailyTransferQuota.Upload.TotalBytes;
+        return (odq > 0, ouq > 0, odq, ouq);
+    }
+}
