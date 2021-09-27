115a484c4104dbb6e7f0767f56483da47f63c246
Diego García <dagarciam1014@gmail.com>
2021-09-27 16:12:45 -0400

    Create DataOperationResults.cs

---
+++ D_API/Types/DataKeeper/DataOperationResults.cs
@@ -0,0 +1,43 @@
+﻿namespace D_API.Types.DataKeeper;
+
+public enum DataOpResult
+{
+    /// <summary>
+    /// The client tried to perform an operation, but had passed their transfer quota for the upload or download operation
+    /// </summary>
+    OverTransferQuota,
+
+    /// <summary>
+    /// The client tried to perform an operation, but had passed their storage quota
+    /// </summary>
+    OverStorageQuota,
+
+    /// <summary>
+    /// The client tried to overwrite data they did not specifically request to overwrite
+    /// </summary>
+    NoOverwrite, 
+
+    /// <summary>
+    /// The client tried to access data that does not exist
+    /// </summary>
+    DataDoesNotExist,
+
+    /// <summary>
+    /// The client tried to access data they do not have access to
+    /// </summary>
+    DataInaccessible,
+
+    /// <summary>
+    /// The client tried to perform an operation without the proper arguments
+    /// </summary>
+    BadArguments,
+
+    /// <summary>
+    /// The operation was succesful
+    /// </summary>
+    Success
+}
+
+public record DataOperationResults(DataOpResult Result) { }
+public record DataOperationResults<T>(DataOpResult Result, T Value) { }
+public record DataOperationResults<T1,T2>(DataOpResult Result, T1 FirstValue, T2 SecondValue) { }
