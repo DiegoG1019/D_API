6594a2252a065d1f1481af89746bd04bf3e64a1d
Diego García <dagarciam1014@gmail.com>
2021-09-27 16:11:49 -0400

    Create IJwtProvider.cs

---
+++ D_API/Dependencies/Interfaces/IJwtProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D_API.Dependencies.Interfaces;
+
+public interface IJwtProvider
+{
+    /// <summary>
+    /// Generates a new token using the provided name and roles
+    /// </summary>
+    /// <param name="names"></param>
+    /// <param name="role"></param>
+    /// <returns></returns>
+    string? GenerateToken(string name, Guid key, TimeSpan duration, params string[] role);
+
+    /// <summary>
+    /// Generates a new token using the provided name and comma separated roles
+    /// </summary>
+    /// <param name="names"></param>
+    /// <param name="role"></param>
+    /// <returns></returns>
+    string? GenerateToken(string name, Guid key, TimeSpan duration, string role);
+}
