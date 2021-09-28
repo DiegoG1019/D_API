4026b94e7ec6284a9aa7cd65e703f816cf1dbc8e
Diego García <dagarciam1014@gmail.com>
2021-09-28 02:45:20 -0400

    Update DataEntry.cs

--- D_API/Models/DataKeeper/DataEntry.cs
+++ D_API/Models/DataKeeper/DataEntry.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using D_API.Models.Auth;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -8,10 +9,10 @@ namespace D_API.Models.DataKeeper
 {
     public class DataEntry
     {
-        public List<Guid> Readers { get; init; } = new();
+        public List<ClientHandle> Readers { get; init; }
 
         [Key, Column(Order = 0)]
-        public Guid Owner { get; init; } = new();
+        public Guid Owner { get; init; }
 
         [Key, Column(Order = 1)]
         public string Identifier { get; init; }
@@ -24,7 +25,7 @@ namespace D_API.Models.DataKeeper
         public double Size => Data?.Length ?? 0;
 
         [NotMapped]
-        public IEnumerable<Guid> ReadAccess => Readers.Prepend(Owner);
+        public IEnumerable<Guid> ReadAccess => Readers.Select(x => x.Key).Prepend(Owner);
 
         /// <summary>
         /// If the Data is null or empty, it's not important
@@ -32,13 +33,15 @@ namespace D_API.Models.DataKeeper
         [NotMapped]
         public bool IsImportant => Size > 0;
 
-        public DataEntry(Guid owner, string identifier, byte[] dat, Guid readonlykey, params Guid[] readers)
+        private DataEntry(Guid owner, string identifier, byte[] data, Guid readOnlyKey) 
+            : this(owner, identifier, data, readOnlyKey, new()) { }
+        public DataEntry(Guid owner, string identifier, byte[] data, Guid readOnlyKey, List<ClientHandle> readers)
         {
-            Readers.AddRange(readers);
+            Readers = readers;
             Owner = owner;
             Identifier = identifier;
-            ReadOnlyKey = readonlykey;
-            Data = dat;
+            ReadOnlyKey = readOnlyKey;
+            Data = data;
         }
     }
 }
