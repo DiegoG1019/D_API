4db9bb7124bf28d8804fec91f19d12b1f07d198b
Diego García <dagarciam1014@gmail.com>
2021-09-28 02:45:30 -0400

    Create ClientHandle.cs

---
+++ D_API/Models/DataKeeper/ClientHandle.cs
@@ -0,0 +1,23 @@
+﻿using D_API.Models.Auth;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D_API.Models.DataKeeper
+{
+    public class ClientHandle
+    {
+        [Key]
+        public Guid Key { get; init; }
+        public string Identifier { get; init; }
+
+        public ClientHandle(Guid key, string identifier)
+        {
+            Key = key;
+            Identifier = identifier;
+        }
+        public ClientHandle(Client client) : this(client.Key, client.Identifier) { }
+    }
+}
