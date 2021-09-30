1b0650a4607c046c201d9fa26ce0b916a335626a
Diego García <dagarciam1014@gmail.com>
2021-09-30 00:46:02 -0400

    Created TransferReport

---
+++ D_API/Types/DataKeeper/TransferReport.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D_API.Types.DataKeeper
+{
+    public record TransferReport(double Upload, double Download)
+    {
+        public TransferReport((double up, double down) tuple) : this(tuple.up, tuple.down) { }
+    }
+}
