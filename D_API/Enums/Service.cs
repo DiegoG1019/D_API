11f0ef5ed8908e5e28e1a426019ef459c560ad83
Diego García <dagarciam1014@gmail.com>
2021-09-30 18:09:33 -0400

    Created Service enum

---
+++ D_API/Enums/Service.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_API.Enums
+{
+    [Flags]
+    public enum Service
+    {
+        Users = 1 << 0,
+        Authorization = 1 << 1,
+        Data = 1 << 2,
+    }
+
+}
