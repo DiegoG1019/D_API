791c217a1624b4211cf97cc6f5d3227fea3bfce5
Diego García <dagarciam1014@gmail.com>
2021-09-30 00:45:25 -0400

    Renovated UserDataTracker

--- D_API/Models/DataKeeper/UserDataTracker.cs
+++ D_API/Models/DataKeeper/UserDataTracker.cs
@@ -1,40 +1,83 @@
-﻿using D_API.Models.DataKeeper.DataUsage.Complex;
+﻿using D_API.Types.DataKeeper;
+using DiegoG.Utilities;
 using DiegoG.Utilities.Collections;
+using NeoSmart.AsyncLock;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace D_API.Models.DataKeeper
 {
     public class UserDataTracker
     {
+        private readonly AsyncLock sync = new();
+        
         [Key]
         public Guid Key { get; set; }
 
-        public UsageTracker DailyTransferQuota { get; set; } = new();
+        #region Transfer
+        
+        public double DailyTransferUploadQuota { get; set; }
+        public double DailyTransferDownloadQuota { get; set; }
 
-        public DailyUsageTracker DailyTransferUsage { get; set; } = new();
+        public TransferReport GetTransferQuota()
+            => new(DailyTransferUploadQuota, DailyTransferDownloadQuota);
 
-        public DataMeasure StorageQuota { get; set; } = new();
+        public List<DailyUsageTracker> Trackers { get; set; }
 
-        public DataMeasure StorageUsage { get; set; } = new();
-
-        [NotMapped]
-        public bool IsOverStorageQuota => StorageUsage >= StorageQuota;
-        public double WouldSurpassStorageQuota(double data) => WouldSurpassStorageQuota(new DataMeasure(data));
-        public double WouldSurpassStorageQuota(DataMeasure data)
+        public async Task ClearOutdatedTransferTrackers()
         {
-            var r = StorageUsage + data - StorageQuota;
-            return r > DiegoG.Utilities.Measures.Data.Zero ? r.TotalBytes : 0;
+            DailyUsageTracker tr;
+            using (await sync.LockAsync())
+                for (int i = 0; i < Trackers.Count; i++)
+                    if ((tr = Trackers[i]).Created + tr.Expiration < DateTime.Now)
+                        Trackers.RemoveAt(i--);
+        }
+
+        public async Task AddTracker(DailyUsageTracker tracker)
+        {
+            using (await sync.LockAsync())
+                Trackers.Add(tracker);
+        }
+
+        public async Task<TransferReport> GetTotalTransferUsage()
+        {
+            using (await sync.LockAsync())
+                return new(Trackers.Aggregate<DailyUsageTracker, (double up, double down)>((0, 0), (acc, val) => (acc.up + val.Upload, acc.down + val.Download)));
         }
 
         public async Task<(bool IsOverDownload, bool IsOverUpload, double OverDownloadQuota, double OverUploadQuota)> GetIfOverTransferQuota()
         {
-            var total = await DailyTransferUsage.GetTotal();
-            double odq = total.Download.TotalBytes - DailyTransferQuota.Download.TotalBytes;
-            double ouq = total.Upload.TotalBytes - DailyTransferQuota.Upload.TotalBytes;
+            var total = await GetTotalTransferUsage();
+            double odq = total.Download - DailyTransferDownloadQuota;
+            double ouq = total.Upload - DailyTransferUploadQuota;
             return (odq > 0, ouq > 0, odq, ouq);
         }
+
+        #endregion
+
+        #region Storage
+
+        /// <summary>
+        /// In Bytes
+        /// </summary>
+        public double StorageQuota { get; set; }
+
+        /// <summary>
+        /// In Bytes
+        /// </summary>
+        public double StorageUsage { get; set; }
+
+        [NotMapped]
+        public bool IsOverStorageQuota => StorageUsage >= StorageQuota;
+
+        public double WouldSurpassStorageQuota(double data) => 0d.Min(StorageUsage + data - StorageQuota);
+
+        #endregion
+
+        public UserDataTracker() { }// => Trackers = Task.Run(() => SerializedTrackers.Select(x => DailyUsageTracker.Deserialize(x)).ToList());
     }
 }
