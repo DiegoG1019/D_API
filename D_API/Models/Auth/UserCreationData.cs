6ebddefcabd01f428dabd00a184b32caa5cbea92
Diego García <dagarciam1014@gmail.com>
2021-09-30 18:10:26 -0400

    Update UserCreationData.cs

--- D_API/Models/Auth/UserCreationData.cs
+++ D_API/Models/Auth/UserCreationData.cs
@@ -5,8 +5,5 @@ using System.Threading.Tasks;
 
 namespace D_API.Models.Auth
 {
-    public class UserCreationData
-    {
-
-    }
+    public record UserCreationData(string Identifier, string Roles) { }
 }
