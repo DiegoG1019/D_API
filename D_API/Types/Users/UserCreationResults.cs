a20f7d2d91226f2912f6f8eaf5c72108ec078e9c
Diego García <dagarciam1014@gmail.com>
2021-09-30 18:14:04 -0400

    Added Service configuration records

--- D_API/Types/Users/UserCreationResults.cs
+++ D_API/Types/Users/UserCreationResults.cs
@@ -8,4 +8,17 @@ using System.Threading.Tasks;
 namespace D_API.Types.Users
 {
     public record UserCreationResults(UserCreationResult? Result, UserValidCredentials? Credentials, string? ReasonForDenial = null);
+    {
+        public IEnumerable<UserServiceData>? ServiceData { get; init; }
+    }
+
+    public abstract record UserServiceData(Service Service);
+    public record UserDataKeeperData(double UploadQuota, double DownloadQuota, double Storage) : UserServiceData(Service.Data);
+
+    public enum UserCreationResult
+    {
+        Accepted,
+        AlreadyExists,
+        Denied
+    }
 }
