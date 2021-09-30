9040e3d27ff26232896573508c511fc091d4a5e3
Diego García <dagarciam1014@gmail.com>
2021-09-30 00:46:14 -0400

    Updated MiscRoutines

--- MiscRoutines/Program.cs
+++ MiscRoutines/Program.cs
@@ -3,26 +3,33 @@ using D_API.Models.Auth;
 using D_API.Types.Auth;
 using DiegoG.Utilities.IO;
 using System.Text;
+using D_API.Models.DataKeeper;
+using System;
+using System.IO;
+using MessagePack;
 
 string hashkey = "&fCd>2Cpxz=@SK>^sQkt5zV0aE]8IKNsqazFOPb:m-RBq0VsBSN?Ebn&^aiO6wE@";
 
-// Generate a new secret and its hashed countepart
-var secret = Helper.GenerateUnhashedSecret();
-var hashedsecret = Helper.GetHash(secret, hashkey);
+#region Generate a new secret and its hashed countepart
+//var secret = Helper.GenerateUnhashedSecret();
+//var hashedsecret = Helper.GetHash(secret, hashkey);
+#endregion
 
-// Generate a new client file
-/*
-File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\aasdw.txt", clientid, System.Text.Encoding.UTF8);
+#region Generate a new client file
 
-var client = await Serialization.Serialize.JsonAsync(new Client()
-{
-    CurrentStatus = Client.Status.Active,
-    Identifier = "DiegoG",
-    Key = Guid.Parse("10158f3a-77d8-4ddc-847d-db209a070f0c"),
-    Secret = await Helper.GetHashAsync(secret, hashkey),
-    Roles = "root"
-});
+//var user = await Serialization.Serialize.JsonAsync(new User()
+//{
+//    CurrentStatus = User.Status.Active,
+//    Identifier = "DiegoG",
+//    Key = Guid.Parse("10158f3a-77d8-4ddc-847d-db209a070f0c"),
+//    Secret = await Helper.GetHashAsync(secret, hashkey),
+//    Roles = "root"
+//});
 
-File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\aasdw.txt", client, Encoding.UTF8);*/
+#endregion
 
-;//place a breakpoint here
+#region Test DailyUsageTracker
+//var x = new DailyUsageTracker(15, 2);
+//var serialx = x.Serialize();
+//var deserialx = DailyUsageTracker.Deserialize(serialx);
+#endregion
