87559116472faba4497365b42239976e07b00406
Diego García <dagarciam1014@gmail.com>
2021-09-30 18:09:54 -0400

    Created Service Enum Extensions

--- D_API/Enums/Service.cs
+++ D_API/Enums/Service.cs
@@ -14,4 +14,41 @@ namespace D_API.Enums
         Data = 1 << 2,
     }
 
+    public static class EnumExtensions
+    {
+        private readonly static Service[] ServiceValues = Enum.GetValues<Service>();
+        public static IEnumerable<Service> Services => ServiceValues;
+
+        private static string AuthRoles(Service service)
+            => service switch
+            {
+                Service.Data => AuthorizationRoles.AppDataHost,
+                _ => $"The service {service} does not have an attached Role or is not supported"
+            };
+
+        private static string UserRoles(Service service)
+            => service switch
+            {
+                Service.Data => UserAccessRoles.AppDataHost,
+                _ => $"The service {service} does not have an attached Role or is not supported"
+            };
+
+        public static string GetUserRoles(this Service service)
+        {
+            var sb = new List<string>(ServiceValues.Length);
+            foreach (var s in ServiceValues)
+                if (service.HasFlag(s))
+                    sb.Add(UserRoles(s));
+            return string.Join(",", sb);
+        }
+
+        public static string GetAuthenticationRoles(this Service service)
+        {
+            var sb = new List<string>(ServiceValues.Length);
+            foreach (var s in ServiceValues)
+                if (service.HasFlag(s))
+                    sb.Add(AuthRoles(s));
+            return string.Join(",", sb);
+        }
+    }
 }
