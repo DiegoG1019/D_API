53a73abbf6c83fec3dfee0fabc137539e80fbed0
Diego García <dagarciam1014@gmail.com>
2021-09-30 23:13:34 -0400

    Update UserCreationData.cs

--- D_API/Models/Auth/UserCreationData.cs
+++ D_API/Models/Auth/UserCreationData.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using D_API.Types.Users;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace D_API.Models.Auth
 {
-    public record UserCreationData(string Identifier, string Roles) { }
+    public record UserCreationData(string Identifier, string Roles, List<SerializedUserServiceData>? Data = null) { }
 }
