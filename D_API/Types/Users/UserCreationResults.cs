d6f13c32bae41447438476afd3f9634caac8914e
Diego García <dagarciam1014@gmail.com>
2021-09-30 23:14:04 -0400

    Updated User Service Configuration classes and conversion

--- D_API/Types/Users/UserCreationResults.cs
+++ D_API/Types/Users/UserCreationResults.cs
@@ -1,5 +1,7 @@
-﻿using D_API.Enums;
+﻿using D_API.Dependencies.Interfaces;
+using D_API.Enums;
 using D_API.Types.Auth;
+using DiegoG.Utilities.Measures;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +11,26 @@ namespace D_API.Types.Users
 {
     public record UserCreationResults(UserCreationResult? Result, UserValidCredentials? Credentials, string? ReasonForDenial = null)
     {
-        public IEnumerable<UserServiceData>? ServiceData { get; init; }
+        public IEnumerable<SerializedUserServiceData>? ServiceData { get; init; }
+        public List<string> ServiceConfigurationResults { get; } = new();
     }
 
-    public abstract record UserServiceData(Service Service);
-    public record UserDataKeeperData(double UploadQuota, double DownloadQuota, double Storage) : UserServiceData(Service.Data);
+    public struct SerializedUserServiceData
+    {
+        public Service Service { get; init; }
+        public object[] Data { get; init; }
+
+        public SerializedUserServiceData(Service service, object[] data)
+        {
+            Service = service;
+            Data = data;
+        }
+
+        public SUSDataConversionResults<IAppDataKeeper.DataKeeperQuotaSettings> ToDataKeeperQuotaSettings()
+            => Data.Length >= 3 ? new(new(Data[0] as double?, Data[1] as double?, Data[2] as double?), true) : new(null, false);
+    }
+
+    public record SUSDataConversionResults<T>(T? Data, bool Success);
 
     public enum UserCreationResult
     {
