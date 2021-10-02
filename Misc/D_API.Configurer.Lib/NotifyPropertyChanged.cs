8722f4f1f5955173458687124e70ae613b7b4193
Diego García <dagarciam1014@gmail.com>
2021-10-02 15:45:26 -0400

    Created SettingsConfigurer classes

---
+++ Misc/D_API.Configurer.Lib/NotifyPropertyChanged.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_API.Configurer.Lib
+{
+    public abstract class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void Notify([CallerMemberName] string? prop = null)
+            => PropertyChanged?.Invoke(this, new(prop));
+        protected void NotifySet<T>(ref T prop, T value, [CallerMemberName] string? propname = null)
+        {
+            prop = value;
+            PropertyChanged?.Invoke(this, new(propname));
+        }
+        
+        protected internal NotifyPropertyChanged() { }
+    }
+}
---
+++ Misc/D_API.Configurer.Lib/SettingsConfigurer.cs
@@ -0,0 +1,63 @@
+﻿using D_API.SettingsTypes;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace D_API.Configurer.Lib
+{
+    public class APISettingsConfigBuilder : NotifyPropertyChanged
+    {
+        private string? encryptioniv;
+        private string? encryptionkey;
+        private long? eventChannelId;
+        private string? jwtKey;
+        private string? telegramKey;
+        private string? hashKey;
+
+        public virtual ObservableCollection<long> AllowedUsers { get; } = new();
+        public virtual APISettingsSecurityBuilder Security { get; } = new();
+        public virtual APISettingsDbConnectionBuilder Database { get; } = new();
+        public virtual string? EncryptionIV { get => encryptioniv; set => NotifySet(ref encryptioniv, value); }
+        public virtual string? EncryptionKey { get => encryptionkey; set => NotifySet(ref encryptionkey, value); }
+        public virtual long? EventChannelId { get => eventChannelId; set => NotifySet(ref eventChannelId, value); }
+        public virtual string? JWTKey { get => jwtKey; set => NotifySet(ref jwtKey, value); }
+        public virtual string? TelegramAPIKey { get => telegramKey; set => NotifySet(ref telegramKey, value); }
+        public virtual string? HashKey { get => hashKey; set => NotifySet(ref hashKey, value); }
+
+        public virtual APISettings Build() => new()
+        {
+            AllowedUsers = new(AllowedUsers),
+            EncryptionIV = encryptioniv,
+            EncryptionKey = encryptionkey,
+            EventChannelId = eventChannelId,
+            JWTSecurityKey = jwtKey,
+            Security = Security.Build(),
+            TelegramAPIKey = telegramKey,
+            UserSecretHashKey = hashKey,
+            UserDataDbConnectionSettings = Database.Build()
+        };
+
+        public class APISettingsDbConnectionBuilder : NotifyPropertyChanged
+        {
+            private DbEndpoint endpoint;
+            private string? connstring;
+
+            public virtual DbEndpoint Endpoint { get => endpoint; set => NotifySet(ref endpoint, value); }
+            public virtual string? ConnectionString { get => connstring; set => NotifySet(ref connstring, value); }
+
+            public virtual DbConnectionSettings Build() => new(endpoint, connstring);
+        }
+
+        public class APISettingsSecurityBuilder : NotifyPropertyChanged
+        {
+            private string? audience;
+            private string? issuer;
+
+            public virtual string? Audience { get => audience; set => NotifySet(ref audience, value); }
+            public virtual string? Issuer { get => audience; set => NotifySet(ref issuer, value); }
+
+            public virtual Security Build() => new(audience, issuer);
+        }
+    }
+}
