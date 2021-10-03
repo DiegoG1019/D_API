89b70a39272e654bf16154e9352c3276021ad72c
Diego García <dagarciam1014@gmail.com>
2021-10-03 19:44:22 -0400

    Create Enums.cs

---
+++ D_API.Lib/Internal/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D_API.Lib.Internal
+{
+    public enum Endpoint
+    {
+        Data,
+        Auth,
+        User
+    }
+
+    public enum Service
+    {
+        Data
+    }
+}
