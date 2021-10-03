d685521a4791632d6a167173e363fa05b81e1b0c
Diego García <dagarciam1014@gmail.com>
2021-10-03 19:44:20 -0400

    Create Credentials.cs

---
+++ D_API.Lib/Models/Credentials.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D_API.Lib.Models
+{
+    public sealed class Credentials
+    {
+        public string UserKey { get; set; }
+        public string Identifier { get; set; }
+        public string Secret { get; set; }
+
+        public Credentials(string userKey, string identifier, string secret)
+        {
+            UserKey = userKey;
+            Identifier = identifier;
+            Secret = secret;
+        }
+
+        public Credentials(Guid userKey, string identifier, string secret) : this(userKey.ToString(), identifier, secret) { }
+    }
+}
