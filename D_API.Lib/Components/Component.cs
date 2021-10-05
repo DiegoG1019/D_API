9bd62a4ef6577845126fb20160abd2dbeb8bdba5
Diego García <dagarciam1014@gmail.com>
2021-10-04 23:37:17 -0400

    Created Component Abstract Class

---
+++ D_API.Lib/Components/Component.cs
@@ -0,0 +1,22 @@
+﻿using D_API.Lib.Internal;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace D_API.Lib.Components
+{
+    public abstract class Component
+    {
+        protected readonly D_APIClient Client;
+        protected readonly IRequestQueue RequestQueue;
+        protected readonly HttpClient Http;
+
+        internal Component(D_APIClient client)
+        {
+            Client = client;
+            RequestQueue = client.RequestQueue;
+            Http = client.Http;
+        }
+    }
+}
