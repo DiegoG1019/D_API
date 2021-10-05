34078c9477e1d500f45c4e1cd48607cb10302c28
Diego García <dagarciam1014@gmail.com>
2021-10-04 23:38:17 -0400

    Created UserManagement Endpoint Handler

---
+++ D_API.Lib/Components/UserManagement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace D_API.Lib.Components
+{
+    public class UserManagement : Component
+    {
+        internal UserManagement(D_APIClient client) : base(client) { }
+
+        [Obsolete("This method is not implemented yet. Calling this method will immediately result in a NotImplementedException throw")]
+        public Task CreateUser() => throw new NotImplementedException();
+    }
+}
