dfd8c4f942a57e22d07c20914a1c5fbe432fc798
Diego García <dagarciam1014@gmail.com>
2021-10-04 23:37:53 -0400

    Created Auth Endpoint Handler

---
+++ D_API.Lib/Components/Auth.cs
@@ -0,0 +1,16 @@
+﻿using D_API.Lib.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_API.Lib.Components
+{
+    public class Auth : Component
+    {
+        internal Auth(D_APIClient client) : base(client) { }
+
+        public Task<AuthStatusResponse> Status() 
+            => RequestQueue.NewRequest(async () => (await Client.GetFromAPIAsync("api/v1/auth/status")).APIResponse.As<AuthStatusResponse>(), Internal.Endpoint.Auth);
+    }
+}
